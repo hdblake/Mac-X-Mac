cc62a2099c66deecd821f7ef690535e30bf9161c
= <=>
2023-10-19 14:34:49 -0500

    added fontfamily and color themes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/index.html", "./src/**/*.{js,jsx}"],
-	theme: {
-		colors: {},
-		extend: {},
-	},
-	variants: {},
-	plugins: [],
+  content: ["./src/index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    fontFamily: {
+      header: ["Great Vibes", "cursive"],
+      mainText: ["Raleway", "sans-serif"],
+      accentText: ["Cabin", "sans-serif"],
+    },
+    colors: {
+      main: "#c5d6be",
+      secondary: "#ffffff",
+      accent: "#c292a1",
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
 };
