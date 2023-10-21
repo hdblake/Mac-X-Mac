064c279afc148c8733ea717cbf138c6c0350a998
= <=>
2023-10-21 13:50:04 -0500

    created footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-main absolute bottom-0 w-full p-2 text-center">
+      <p className="text-secondary font-mainText">Mac X Mac 2023&copy;</p>
+    </footer>
+  );
+}
