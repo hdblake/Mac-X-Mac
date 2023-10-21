b3741338b6c4c5bbc6eb1ee86111f5d05ffaf29c
= <=>
2023-10-21 13:51:19 -0500

    updated layout

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,7 +1,15 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Home from "../pages/Home";
+
 export default function App() {
   return (
-    <div>
-      <h1>Mac X Mac</h1>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Home />
+      </main>
+      <Footer />
+    </>
   );
 }
