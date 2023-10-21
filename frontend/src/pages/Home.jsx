ef8415672d39410b5c683926970795329e7d27d1
= <=>
2023-10-21 13:51:39 -0500

    started Home page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="font-header text-main text-9xl text-center mt-8">
+        Welcome
+      </h1>
+      <p className="text-center font-mainText text-main text-lg w-3/5 m-auto mt-5">
+        Hello and welcome to Mac-X-Mac (Mac by Mac). Below are some examples of
+        items I have created. Please feel free to explore the rest of the
+        website! If you don't see something you are looking for, don't hesitate
+        to reach out with any questions or inquiries utilzing the provided
+        contact form!
+      </p>
+    </main>
+  );
+}
