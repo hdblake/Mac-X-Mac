2d2365b6e9bb452e728d20d30ca570498432bd58
= <=>
2023-10-23 17:10:52 -0500

    created images data

---
+++ frontend/src/data/images.js
@@ -0,0 +1,38 @@
+import blackKeychain from "../images/black-keychain.webp";
+import ornaments from "../images/ornaments.webp";
+import wallHang from "../images/wall-hang.webp";
+import nameSigns from "../images/name-signs.webp";
+import ourWedding from "../images/our-wedding.webp";
+import fullWedding from "../images/full-wedding.webp";
+import redWedding from "../images/red-wedding-signs.webp";
+import whiteWedding from "../images/white-wedding-sign.webp";
+import redGreenSign from "../images/red-green-sign.webp";
+import keychainTrio from "../images/three-keychains.webp";
+
+export const carouselImages = [
+  {
+    id: 1,
+    description: "multi-color keychains",
+    image: keychainTrio,
+  },
+  {
+    id: 2,
+    description: "macrame ornaments",
+    image: ornaments,
+  },
+  {
+    id: 3,
+    description: "signs with names",
+    image: nameSigns,
+  },
+  {
+    id: 4,
+    description: "green sign from our wedding",
+    image: ourWedding,
+  },
+  {
+    id: 5,
+    description: "macrame wall hang",
+    image: wallHang,
+  },
+];
