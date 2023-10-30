665a5eb7893af5c76a554ec5b87e6bd18ecb0c33
= <=>
2023-10-30 12:59:41 -0500

    added pages with routes

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,15 +1,24 @@
+import { Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import Home from "../pages/Home";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Products from "../pages/Products";
 
 export default function App() {
   return (
-    <>
+    <div className="flex flex-col h-screen justify-between">
       <Header />
-      <main>
-        <Home />
+      <main className="mb-auto">
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path="/About" element={<About />}></Route>
+          <Route path="/Contact" element={<Contact />}></Route>
+          <Route path="/Products" element={<Products />}></Route>
+        </Routes>
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
