ec4bf2d76d2463249a398c10b2c9b6ffa3525879
= <=>
2023-10-30 13:00:04 -0500

    updated and added Link

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom";
 import mainLogo from "../images/macxmac.png";
 
 export default function Header() {
   return (
     <header>
       <div className="flex justify-center align-center items-center p-3">
-        <a href="#home">
+        <Link to="/">
           <img src={mainLogo} alt="logo" width={200} height={200} />
-        </a>
+        </Link>
       </div>
       <div className="bg-main p-2 text-center">
         <span className="font-mainText font-bold text-secondary text-xl">
@@ -20,15 +21,21 @@ export default function Header() {
       <div className="bg-main">
         <nav>
           <ul className="fixed left-0 top-1/2">
-            <li className="my-2 ml-4 text-main font-mainText font-bold text-2xl hover:underline hover:decoration-4 hover:decoration-accent hover:underline-offset-4">
-              Products
-            </li>
-            <li className="my-2 ml-4 text-main font-mainText font-bold text-2xl hover:underline hover:decoration-4 hover:decoration-accent hover:underline-offset-4">
-              About
-            </li>
-            <li className="my-2 ml-4 text-main font-mainText font-bold text-2xl hover:underline hover:decoration-4 hover:decoration-accent hover:underline-offset-4">
-              Contact
-            </li>
+            <Link to="/Products">
+              <li className="my-2 ml-4 text-main font-mainText font-bold text-2xl hover:underline hover:decoration-4 hover:decoration-accent hover:underline-offset-4">
+                Products
+              </li>
+            </Link>
+            <Link to="/About">
+              <li className="my-2 ml-4 text-main font-mainText font-bold text-2xl hover:underline hover:decoration-4 hover:decoration-accent hover:underline-offset-4">
+                About
+              </li>
+            </Link>
+            <Link to="/Contact">
+              <li className="my-2 ml-4 text-main font-mainText font-bold text-2xl hover:underline hover:decoration-4 hover:decoration-accent hover:underline-offset-4">
+                Contact
+              </li>
+            </Link>
           </ul>
         </nav>
       </div>
