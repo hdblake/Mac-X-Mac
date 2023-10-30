b49e48c6895549fddd9b199f348fbfdf93eecea4
= <=>
2023-10-30 13:00:32 -0500

    created contact form page

---
+++ frontend/src/pages/Contact.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import ContactForm from "../components/ContactForm";
+
+export default function Contact() {
+  useEffect(() => {
+    document.title = "Mac X Mac | Contact";
+  });
+  return (
+    <section>
+      <h1 className="font-header text-main text-8xl text-center mt-10 underline decoration-2 decoration-accent">
+        Contact
+      </h1>
+      <p className="text-center font-mainText text-main text-lg w-3/5 m-auto mt-6">
+        Please use the form below to send me any inquires or questions you might
+        have. I will do my best to respond in a timely manner!
+      </p>
+      <ContactForm />
+    </section>
+  );
+}
