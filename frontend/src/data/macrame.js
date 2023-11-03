189c8689bd9d5c82141d61c2ff64f05604a0e4ca
= <=>
2023-11-03 14:35:57 -0500

    created data for images

---
+++ frontend/src/data/macrame.js
@@ -0,0 +1,35 @@
+import wallHang from "../images/wall-hang-2.webp";
+import keychains from "../images/three-keychains.webp";
+import ornament from "../images/ornament.webp";
+
+export const macrame = [
+  {
+    id: 1,
+    name: "Ornaments",
+    price: 9.99,
+    colors: ["Pink", "Black", "Cream"],
+    image: ornament,
+  },
+  {
+    id: 2,
+    name: "Keychains",
+    price: 4.99,
+    colors: [
+      "White/Gold",
+      "Black",
+      "Olive Green",
+      "Pink",
+      "Purple",
+      "Cream",
+      "Tan",
+    ],
+    image: keychains,
+  },
+  {
+    id: 3,
+    name: "Wall Hangs",
+    price: 19.99,
+    colors: ["Cream", "Black"],
+    image: wallHang,
+  },
+];
