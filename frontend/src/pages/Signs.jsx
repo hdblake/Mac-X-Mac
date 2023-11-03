14de7c2908f43b3ee82193d6ca56c1a989035d6d
= <=>
2023-11-03 14:34:38 -0500

    created file

---
+++ frontend/src/pages/Signs.jsx
@@ -0,0 +1,9 @@
+export default function Signs() {
+  return (
+    <section>
+      <h1 className="font-header text-main text-8xl text-center mt-10 underline decoration-2 decoration-accent">
+        Signs
+      </h1>
+    </section>
+  );
+}
