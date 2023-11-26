1155357ca99eba1123cce1adb74d05beac32731a
= <=>
2023-11-25 20:19:24 -0600

    added cart link

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -36,6 +36,11 @@ export default function Header() {
                 Contact
               </li>
             </Link>
+            <Link to="/Cart">
+              <li className="my-2 ml-4 text-main font-mainText font-bold text-2xl hover:underline hover:decoration-4 hover:decoration-accent hover:underline-offset-4">
+                Cart
+              </li>
+            </Link>
           </ul>
         </nav>
       </div>
