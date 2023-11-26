9a255d2a4ca404e1614547ba248dcd51257b91bd
= <=>
2023-11-25 23:16:51 -0600

    made more responive

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -11,24 +11,24 @@ export default function Navigation() {
   return (
     <nav>
       <div className={`menu ${isOpen ? "open" : ""}`}>
-        <ul className="absolute top-72 md:top-1/2 flex flex-row flex-wrap items-center justify-center md:flex-col md:left-0 md:items-start">
+        <ul className="absolute top-72 md:top-1/2 flex flex-row flex-wrap items-center justify-center md:fixed md:flex-col md:pr-6 md:left-0 md:items-start">
           <Link to="/Products">
-            <li className="my-2 ml-4 text-main font-mainText font-bold text-xl lg:text-2xl hover:underline hover:decoration-4 hover:decoration-accent hover:underline-offset-4">
+            <li className="my-2 ml-4 lg:ml-2 text-main font-mainText font-bold text-xl lg:text-2xl hover:underline hover:decoration-4 hover:decoration-accent hover:underline-offset-4">
               Products
             </li>
           </Link>
           <Link to="/About">
-            <li className="my-2 ml-4 text-main font-mainText font-bold text-xl lg:text-2xl hover:underline hover:decoration-4 hover:decoration-accent hover:underline-offset-4">
+            <li className="my-2 ml-4 lg:ml-2 text-main font-mainText font-bold text-xl lg:text-2xl hover:underline hover:decoration-4 hover:decoration-accent hover:underline-offset-4">
               About
             </li>
           </Link>
           <Link to="/Contact">
-            <li className="my-2 ml-4 text-main font-mainText font-bold text-xl lg:text-2xl hover:underline hover:decoration-4 hover:decoration-accent hover:underline-offset-4">
+            <li className="my-2 ml-4 lg:ml-2 text-main font-mainText font-bold text-xl lg:text-2xl hover:underline hover:decoration-4 hover:decoration-accent hover:underline-offset-4">
               Contact
             </li>
           </Link>
           <Link to="/Cart">
-            <li className="my-2 ml-4 text-main font-mainText font-bold text-xl lg:text-2xl hover:underline hover:decoration-4 hover:decoration-accent hover:underline-offset-4">
+            <li className="my-2 ml-4 lg:ml-2 text-main font-mainText font-bold text-xl lg:text-2xl hover:underline hover:decoration-4 hover:decoration-accent hover:underline-offset-4">
               Cart
             </li>
           </Link>
