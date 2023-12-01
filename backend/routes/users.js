2b7b9c03b92f2207a2e912b66cddaca49615d81d
= <=>
2023-11-30 19:30:37 -0600

    created

---
+++ backend/routes/users.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const userController = require("../controllers/users");
+
+router.post("/register", userController.registerUser);
+router.post("/login", userController.loginUser);
+
+module.exports = router;
