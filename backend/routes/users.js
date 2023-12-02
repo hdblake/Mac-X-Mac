2e42002ac75cde1210d9115d06ed251264d3fda8
= <=>
2023-12-01 23:01:43 -0600

    added route for account

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,5 +3,11 @@ const userController = require("../controllers/users");
 
 router.post("/register", userController.registerUser);
 router.post("/login", userController.loginUser);
+router.get("/account", userController.verifyToken, (req, res) => {
+	res.json({
+		message: "This is a protected route",
+		user: req.decoded,
+	});
+});
 
 module.exports = router;
