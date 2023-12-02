475243e3da76617d3b0910aefd3bd226f7c97baf
= <=>
2023-12-01 23:02:27 -0600

    added Account and functionality

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import Home from "../pages/Home";
@@ -10,22 +10,57 @@ import Signs from "../pages/Signs";
 import Cart from "../pages/Cart";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
+import { useState, useEffect } from "react";
+import Account from "../pages/Account";
 
 export default function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setLoggedIn(true);
+    }
+  }, []);
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("firstName");
+    setLoggedIn(false);
+    navigate("/");
+  };
+
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
   return (
     <div className="flex flex-col h-screen justify-between">
-      <Header />
+      <Header loggedIn={loggedIn} logout={logout} />
       <main className="mb-auto">
         <Routes>
           <Route index element={<Home />}></Route>
           <Route path="/About" element={<About />}></Route>
           <Route path="/Contact" element={<Contact />}></Route>
           <Route path="/Products" element={<Products />}></Route>
-          <Route path="/Products/Macrame" element={<Macrame />}></Route>
+          <Route
+            path="/Products/Macrame"
+            element={<Macrame addToCart={addToCart} />}
+          ></Route>
           <Route path="/Products/Signs" element={<Signs />}></Route>
-          <Route path="/Cart" element={<Cart />}></Route>
+          <Route path="/Cart" element={<Cart cart={cart} />}></Route>
           <Route path="/Register" element={<Register />}></Route>
-          <Route path="/Login" element={<Login />}></Route>
+          <Route
+            path="/Login"
+            element={<Login setLoggedIn={setLoggedIn} />}
+          ></Route>
+          <Route
+            path="/Account"
+            element={<Account setLoggedIn={setLoggedIn} />}
+          ></Route>
         </Routes>
       </main>
       <Footer />
