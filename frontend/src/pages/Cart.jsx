a2d166abf65b46b5a648bf749624107d24cc57dc
= <=>
2023-12-04 13:59:42 -0600

    added handleRemove and product details

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,15 +1,32 @@
 import { useEffect } from "react";
 
-export default function Cart() {
+export default function Cart({ cart, removeFromCart }) {
   useEffect(() => {
     document.title = "Mac X Mac | Cart";
   });
+
+  const handleRemove = (index) => {
+    removeFromCart(index);
+  };
+
+  console.log(cart);
 
   return (
     <section>
       <h1 className="font-header text-main text-7xl md:text-8xl text-center mt-10 underline decoration-2 decoration-accent">
         Cart
       </h1>
+      <div className="flex flex-col items-center gap-y-6">
+        {cart.map((product, index) => (
+          <div key={index}>
+            <p>Name: {product.name}</p>
+            <p>Price: ${product.price}</p>
+            <p>Color: {product.color}</p>
+            <p>Quantity: {product.quantity}</p>
+            <button onClick={() => handleRemove(index)}>Remove</button>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
