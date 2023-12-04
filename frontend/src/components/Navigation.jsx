79cbc070e0e04440c3c4f9cc59de8f36a6bd27e5
Hunter <hdblake333@gmail.com>
2023-12-04 15:23:04 -0600

    updated style and positioning

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -9,14 +9,9 @@ export default function Navigation() {
   };
 
   return (
-    <nav>
+    <nav className="relative">
       <div className={`menu ${isOpen ? "open" : ""}`}>
-        <ul className="absolute top-72 md:top-1/2 flex flex-row flex-wrap items-center justify-center md:fixed md:flex-col md:pr-6 md:left-0 md:items-start">
-          <Link to="/Products">
-            <li className="my-2 ml-4 lg:ml-2 text-main font-mainText font-bold text-xl lg:text-2xl hover:underline hover:decoration-4 hover:decoration-accent hover:underline-offset-4">
-              Products
-            </li>
-          </Link>
+        <ul className="absolute md:top-1/2 flex flex-row flex-wrap items-center justify-center md:fixed md:flex-col md:pr-6 md:left-0 md:items-start">
           <Link to="/About">
             <li className="my-2 ml-4 lg:ml-2 text-main font-mainText font-bold text-xl lg:text-2xl hover:underline hover:decoration-4 hover:decoration-accent hover:underline-offset-4">
               About
@@ -25,6 +20,11 @@ export default function Navigation() {
           <Link to="/Contact">
             <li className="my-2 ml-4 lg:ml-2 text-main font-mainText font-bold text-xl lg:text-2xl hover:underline hover:decoration-4 hover:decoration-accent hover:underline-offset-4">
               Contact
+            </li>
+          </Link>
+          <Link to="/Products">
+            <li className="my-2 ml-4 lg:ml-2 text-main font-mainText font-bold text-xl lg:text-2xl hover:underline hover:decoration-4 hover:decoration-accent hover:underline-offset-4">
+              Products
             </li>
           </Link>
           <Link to="/Cart">
