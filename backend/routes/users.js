7ff4c536aa3144766129c545442ed07210ed9fed
Hunter <hdblake333@gmail.com>
2023-12-04 23:40:23 -0600

    changed /account to /auth

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,12 +3,6 @@ const userController = require("../controllers/users");
 
 router.post("/register", userController.registerUser);
 router.post("/login", userController.loginUser);
-router.get("/account", userController.verifyToken, (req, res) => {
-	res.json({
-		message: "This is a protected route",
-		user: req.decoded,
-	});
-});
-router.get("/history/:userId", userController.getOrders);
+router.get("/auth", userController.verifyToken);
 
 module.exports = router;
