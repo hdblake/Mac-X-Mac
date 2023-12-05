81576b4a325ca8ea429122a326c3d9139b914c8d
Hunter <hdblake333@gmail.com>
2023-12-05 01:31:38 -0600

    added verifyToken

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
+const userController = require("../controllers/users");
 const orderController = require("../controllers/orders");
 
-router.post("/checkout", orderController.createOrder);
+router.post(
+	"/checkout",
+	userController.verifyToken,
+	orderController.createOrder
+);
 
 module.exports = router;
