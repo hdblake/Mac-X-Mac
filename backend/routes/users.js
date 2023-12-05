8aa0b29627c9376d5651eae0b85764805c11eb97
Hunter <hdblake333@gmail.com>
2023-12-05 01:31:18 -0600

    added verifyToken

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,6 +3,11 @@ const userController = require("../controllers/users");
 
 router.post("/register", userController.registerUser);
 router.post("/login", userController.loginUser);
-router.get("/auth", userController.verifyToken);
+router.get("/auth", userController.verifyToken, (req, res) => {
+	res.json({
+		message: "You have access to this protected route",
+		user: req.user,
+	});
+});
 
 module.exports = router;
