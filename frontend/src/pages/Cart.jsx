caf62b758a64b754d5ca9f4984781045c5bdd4c1
Hunter <hdblake333@gmail.com>
2023-12-04 18:46:57 -0600

    added functionality for cart being empty

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function Cart({ cart, removeFromCart }) {
   useEffect(() => {
@@ -16,17 +17,51 @@ export default function Cart({ cart, removeFromCart }) {
       <h1 className="font-header text-main text-7xl md:text-8xl text-center mt-10 underline decoration-2 decoration-accent">
         Cart
       </h1>
-      <div className="flex flex-col items-center gap-y-6">
-        {cart.map((product, index) => (
-          <div key={index}>
-            <p>Name: {product.name}</p>
-            <p>Price: ${product.price}</p>
-            <p>Color: {product.color}</p>
-            <p>Quantity: {product.quantity}</p>
-            <button onClick={() => handleRemove(index)}>Remove</button>
-          </div>
-        ))}
-      </div>
+      {cart.length === 0 ? (
+        <div className="text-center">
+          <p className="font-mainText text-main text-md md:text-lg">
+            Cart is empty!
+          </p>
+          <p className="font-mainText text-main text-md md:text-lg">
+            Please add products to cart to checkout
+          </p>
+        </div>
+      ) : (
+        <div className="flex flex-col items-center gap-y-6">
+          {cart.map((product, index) => (
+            <div
+              key={index}
+              className="rounded-xl shadow-lg border border-2 border-accent p-4"
+            >
+              <p className="font-mainText text-main text-md md:text-lg">
+                Name: {product.name}
+              </p>
+              <p className="font-mainText text-main text-md md:text-lg">
+                Price: ${product.price}
+              </p>
+              <p className="font-mainText text-main text-md md:text-lg">
+                Color: {product.color}
+              </p>
+              <p className="font-mainText text-main text-md md:text-lg">
+                Quantity: {product.quantity}
+              </p>
+              <div className="mt-4 flex justify-center">
+                <button
+                  className="font-mainText text-secondary bg-accent p-2 rounded-lg"
+                  onClick={() => handleRemove(index)}
+                >
+                  Remove
+                </button>
+              </div>
+            </div>
+          ))}
+          <Link to="/Checkout">
+            <button className="font-mainText text-secondary bg-accent text-xl p-2 rounded-lg mt-4">
+              Checkout
+            </button>
+          </Link>
+        </div>
+      )}
     </section>
   );
 }
