d8cf1ca05ccc87f205841faabc714dac15be634d
Hunter <hdblake333@gmail.com>
2023-12-04 18:45:04 -0600

    created

---
+++ backend/models/orderModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+	},
+	items: {type: Array},
+	totalPrice: {type: Number},
+	date: {type: Date},
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
