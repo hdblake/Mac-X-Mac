2817318d745a96e04c517d11caa07bd958070cbf
Hunter <hdblake333@gmail.com>
2023-12-04 23:40:05 -0600

    removed index controller

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,5 @@
 const routes = require("express").Router();
-const controller = require("../controllers");
 
-routes.get("/", controller.routesController);
 routes.use("/user", require("./users"));
 routes.use("/orders", require("./orders"));
 
