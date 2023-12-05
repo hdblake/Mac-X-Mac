1532af67359d1c01f0f695864be91e4b3234380f
= <=>
2023-12-05 16:52:26 -0600

    added Orders route

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -14,6 +14,7 @@ import { useState, useEffect } from "react";
 import Account from "../pages/Account";
 import Checkout from "../pages/Checkout";
 import ProtectedRoute from "./ProtectedRoute";
+import Orders from "../pages/Orders";
 
 export default function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -103,6 +104,7 @@ export default function App() {
               />
             }
           ></Route>
+          <Route path="/Account/Orders" element={<Orders />}></Route>
         </Routes>
       </main>
       <Footer />
