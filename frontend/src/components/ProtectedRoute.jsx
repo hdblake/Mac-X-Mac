f085114bb70d6ddbf27535fbb9f5dcd5f3a15d74
Hunter <hdblake333@gmail.com>
2023-12-04 23:41:10 -0600

    created reusable protected component

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,36 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios";
+
+export default function ProtectedRoute({ element: Element, ...rest }) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await axios.get("http://localhost:3080/user/auth", {
+          headers: { Authorization: token },
+        });
+        console.log(response.data.message);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    verifyToken();
+  }, []);
+
+  useEffect(() => {
+    const isAuthorized = !!localStorage.getItem("token");
+    if (!isAuthorized) {
+      navigate("/Login", {
+        state: {
+          message:
+            "Not authorized to view this page. Please login to view page",
+        },
+      });
+    }
+  }, []);
+
+  return <Element {...rest} />;
+}
