48f3269992f5019516092f98a1a82e442a2d1a5d
= <=>
2023-12-06 19:49:28 -0600

    added more functionality

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -2,34 +2,67 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function Orders({ userId }) {
-  const [orderHistory, setOrderHistory] = useState([]);
+  const [orders, setOrders] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     const fetchOrderHistory = async () => {
       try {
-        const response = axios.get(
-          "http://localhost:3080/user/history/${userId}",
+        const token = localStorage.getItem("token");
+        const response = await axios.get(
+          `http://localhost:3080/orders/history/${userId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
         );
-        setOrderHistory(response.data);
-      } catch (err) {
-        console.log("Error fetching order history", err);
+        console.log(response.data);
+        if (response && response.data && response.data.length > 0) {
+          console.log("Orders: ", response.data);
+          setOrders(response.data);
+        } else {
+          setError("No order history found or empty array received");
+        }
+      } catch (error) {
+        console.log("Error fetching order history: ", error.response || error);
+        setError("Error fetching order history");
+      } finally {
+        setLoading(false);
       }
     };
     fetchOrderHistory();
   }, [userId]);
-}
 
-return (
-  <section>
-    <h2>Order History</h2>
-    <ul>
-      {orderHistory.map((order) => (
-        <li key={order._id}>
-          <p>Items: {order.items}</p>
-          <p>Total: {order.totalPrice}</p>
-          <p>Ordered on: {order.date}</p>
-        </li>
-      ))}
-    </ul>
-  </section>
-);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <section>
+      <h2>Order History</h2>
+      <ul>
+        {orders.map((order) => (
+          <li key={order._id}>
+            <p>Items:</p>
+            {order.items.map((item, i) => (
+              <div key={i}>
+                <p>{item.name}</p>
+                <p>{item.quantity}</p>
+                <p>{item.price}</p>
+                <p>{item.color}</p>
+              </div>
+            ))}
+            <p>Total: {order.totalPrice}</p>
+            <p>Ordered on: {order.date}</p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
