92928b83ea912b98c38883bc9f31224450295e92
= <=>
2023-12-07 14:30:52 -0600

    removed underline from h1

--- frontend/src/pages/Signs.jsx
+++ frontend/src/pages/Signs.jsx
@@ -1,7 +1,7 @@
 export default function Signs() {
   return (
     <section>
-      <h1 className="font-header text-main text-8xl text-center mt-10 underline decoration-2 decoration-accent">
+      <h1 className="font-header text-main text-8xl text-center mt-10">
         Signs
       </h1>
     </section>
