8f11e66995961f100c28f7f215f6b234c0ffb536
= <=>
2023-12-08 17:45:47 -0600

    updated and created stripe capabilities

--- backend/controllers/orders.js
+++ backend/controllers/orders.js
@@ -1,18 +1,28 @@
 const Order = require("../models/orderModel");
+const dotenv = require("dotenv");
+dotenv.config();
+const stripe = require("stripe")(process.env.STRIPE_TEST_SECRET_KEY);
 
 const createOrder = async (req, res, next) => {
 	try {
-		const {userId, items, totalPrice, date} = req.body;
+		const {userId, items, totalPrice, date, customerInfo} = req.body;
 
 		const newOrder = new Order({
 			user: userId,
 			items,
 			totalPrice,
 			date,
+			customerInfo,
 		});
 
-		const saveOrder = await newOrder.save();
-		res.json(saveOrder);
+		await newOrder.save();
+
+		const paymentIntent = await stripe.paymentIntents.create({
+			amount: totalPrice * 100,
+			currency: "usd",
+			description: "Order payment",
+		});
+		res.status(200).json({clientSecret: paymentIntent});
 	} catch (error) {
 		res.status(500).json({message: "Failed to submit order"});
 	}
@@ -35,4 +45,23 @@ const getOrders = async (req, res, next) => {
 	}
 };
 
-module.exports = {createOrder, getOrders};
+const makePayment = async (req, res, next) => {
+	try {
+		const {paymentMethod, clientSecret, returnURL} = req.body;
+
+		const paymentIntent = await stripe.paymentIntents.confirm(clientSecret, {
+			payment_method: paymentMethod,
+			return_url: returnURL,
+		});
+
+		if (paymentIntent) {
+			res.status(200).json({message: "Payment succeeded"});
+		} else {
+			res.status(500).json({error: "Payment failed"});
+		}
+	} catch (error) {
+		res.status(500).json({error: error.message});
+	}
+};
+
+module.exports = {createOrder, getOrders, makePayment};
