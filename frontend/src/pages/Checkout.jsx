8021bb90dd0a5f00677228edaacc286b6558dd9a
= <=>
2023-12-08 17:44:49 -0600

    added payment handling from stripe

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
@@ -7,7 +7,14 @@ export default function Checkout({ cart, setCart }) {
     document.title = "Mac X Mac | Checkout";
   });
 
+  const [customerInfo, setCustomerInfo] = useState({});
+  const [paymentMessage, setPaymentMessage] = useState("");
   const navigate = useNavigate();
+
+  const handleCustomerInfo = (event) => {
+    const { name, value } = event.target;
+    setCustomerInfo({ ...customerInfo, [name]: value });
+  };
 
   const shipping = 10 + (cart.length - 1) * 2;
   const total = cart
@@ -20,7 +27,7 @@ export default function Checkout({ cart, setCart }) {
     parseFloat(tax)
   ).toFixed(2);
 
-  const handleCheckout = async () => {
+  const handleOrderAndPayment = async () => {
     try {
       const userId = localStorage.getItem("userId");
       const token = localStorage.getItem("token");
@@ -31,6 +38,7 @@ export default function Checkout({ cart, setCart }) {
           items: cart,
           totalPrice,
           date: new Date(),
+          customerInfo,
         },
         {
           headers: {
@@ -38,13 +46,36 @@ export default function Checkout({ cart, setCart }) {
           },
         },
       );
-      console.log("Order placed:", response.data);
-      localStorage.removeItem("cart");
-      setCart([]);
-      navigate("/", { state: { message: "Order successfully placed!" } });
-    } catch (err) {
-      console.log("Error: " + err);
+      const clientSecret = response.data.clientSecret;
+      console.log(clientSecret);
+
+      const paymentResponse = await axios.post(
+        "http://localhost:3080/orders/process-payment",
+        {
+          paymentMethod: "pm_card_visa",
+          clientSecret: clientSecret.id,
+          returnURL: "http://localhost:5173",
+        },
+      );
+
+      console.log(`Payment response: ${paymentResponse.data}`);
+
+      if (paymentResponse) {
+        setPaymentMessage("Payment successful");
+        console.log("Order placed:", response.data);
+        localStorage.removeItem("cart");
+        setCart([]);
+        navigate("/", { state: { message: "Order successfully placed!" } });
+      } else {
+        setPaymentMessage("Payment failed");
+      }
+    } catch (error) {
+      setPaymentMessage("Payment failed: unknown reason");
     }
+  };
+
+  const handleSubmit = () => {
+    handleOrderAndPayment();
   };
 
   return (
@@ -81,10 +112,87 @@ export default function Checkout({ cart, setCart }) {
                 Order Total: ${totalPrice}
               </p>
             </div>
+            <div className="flex flex-col justify-center ">
+              <label
+                htmlFor="firstName"
+                className="font-mainText text-accent text-lg mr-2 mt-4"
+              >
+                First Name:{" "}
+              </label>
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                className="border border-2 border-main focus:border focus:border-2 focus:border-accent outline-none rounded-lg text-accent font-mainText p-1"
+                value={customerInfo.firstName || ""}
+                onChange={handleCustomerInfo}
+                required
+              />
+              <label
+                htmlFor="lastName"
+                className="font-mainText text-accent text-lg mr-2 mt-4"
+              >
+                Last Name:{" "}
+              </label>
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                className="border border-2 border-main focus:border focus:border-2 focus:border-accent outline-none rounded-lg text-accent font-mainText p-1"
+                value={customerInfo.lastName || ""}
+                onChange={handleCustomerInfo}
+                required
+              />
+              <label
+                htmlFor="address"
+                className="font-mainText text-accent text-lg mr-2 mt-4"
+              >
+                Address:{" "}
+              </label>
+              <input
+                type="text"
+                id="address"
+                name="address"
+                className="border border-2 border-main focus:border focus:border-2 focus:border-accent outline-none rounded-lg text-accent font-mainText p-1"
+                value={customerInfo.address || ""}
+                onChange={handleCustomerInfo}
+                required
+              />
+              <label
+                htmlFor="cityState"
+                className="font-mainText text-accent text-lg mr-2 mt-4"
+              >
+                City, State:{" "}
+              </label>
+              <input
+                type="text"
+                id="cityState"
+                name="cityState"
+                className="border border-2 border-main focus:border focus:border-2 focus:border-accent outline-none rounded-lg text-accent font-mainText p-1"
+                value={customerInfo.cityState || ""}
+                onChange={handleCustomerInfo}
+                required
+              />
+              <label
+                htmlFor="zipcode"
+                className="font-mainText text-accent text-lg mr-2 mt-4"
+              >
+                Zipcode:{" "}
+              </label>
+              <input
+                type="text"
+                id="zipcode"
+                name="zipcode"
+                className="border border-2 border-main focus:border focus:border-2 focus:border-accent outline-none rounded-lg text-accent font-mainText p-1"
+                value={customerInfo.zipcode || ""}
+                onChange={handleCustomerInfo}
+                required
+              />
+            </div>
             <div className="flex justify-center">
               <button
                 className="font-mainText text-secondary bg-accent p-2 rounded-lg mt-4"
-                onClick={handleCheckout}
+                onClick={handleSubmit}
               >
                 Checkout
               </button>
