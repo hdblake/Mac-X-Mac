0bfc0fabcd3221c6be87e2765eccd265afca1e34
= <=>
2023-12-08 01:18:49 -0600

    created data for signs

---
+++ frontend/src/data/signs.js
@@ -0,0 +1,40 @@
+export const sizePricing = [
+  {
+    id: 1,
+    size: "8 x 11",
+    price: 19.99,
+  },
+  {
+    id: 2,
+    size: "11 x 14",
+    price: 34.99,
+  },
+  {
+    id: 3,
+    size: "18 x 24",
+    price: 49.99,
+  },
+];
+
+export const signTypes = [
+  {
+    id: 1,
+    type: "Welcome",
+  },
+  {
+    id: 2,
+    type: "Seating Chart",
+  },
+  {
+    id: 3,
+    type: "Dessert Table",
+  },
+  {
+    id: 4,
+    type: "Bar",
+  },
+  {
+    id: 5,
+    type: "Name",
+  },
+];
