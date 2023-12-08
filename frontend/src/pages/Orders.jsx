28160527fa229b21dc99c97a551a2677e45064f2
= <=>
2023-12-08 02:12:40 -0600

    updated title and handling of no orders

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -2,6 +2,10 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function Orders() {
+  useEffect(() => {
+    document.title = "Mac X Mac | Orders";
+  });
+
   const [orders, setOrders] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
@@ -29,7 +33,7 @@ export default function Orders() {
         }
       } catch (error) {
         console.log("Error fetching order history: ", error.response || error);
-        setError("Error fetching order history");
+        setError("No order history");
       } finally {
         setLoading(false);
       }
@@ -42,7 +46,19 @@ export default function Orders() {
   }
 
   if (error) {
-    return <div>Error: {error}</div>;
+    return (
+      <div>
+        <h1 className="font-header text-main text-7xl md:text-8xl md:px-2 text-center mt-10">
+          Order History
+        </h1>
+        <p className="text-center text-accent font-mainText mt-4">
+          Error: {error}
+        </p>
+        <p className="text-center text-accent font-mainText mt-4">
+          Try placing an order to view order history!
+        </p>
+      </div>
+    );
   }
 
   return (
@@ -61,47 +77,51 @@ export default function Orders() {
             </p>
             {order.items.map((item, i) => (
               <div key={i}>
-                <p>
-                  <span className="font-mainText text-main text-lg">
-                    Product:
-                  </span>
-                  <span className="font-mainText text-main text-lg">
-                    {" "}
-                    {item.name}
-                  </span>
-                </p>
-                <p>
-                  <span className="font-mainText text-main text-lg">
-                    Quantity:
-                  </span>
-                  <span className="font-mainText text-main text-lg">
-                    {" "}
-                    {item.quantity}
-                  </span>
-                </p>
-                <p>
-                  <span className="font-mainText text-main text-lg">
-                    Price:
-                  </span>
-                  <span className="font-mainText text-main text-lg">
-                    {" "}
-                    ${item.price}
-                  </span>
-                </p>
-                <p>
-                  <span className="font-mainText text-main text-lg">
-                    Color:
-                  </span>
-                  <span className="font-mainText text-main text-lg">
-                    {" "}
-                    {item.color}
-                  </span>
-                </p>
+                {Object.keys(item).length <= 4 && (
+                  <div className="border-t-4 border-accent">
+                    <p className="font-mainText text-main text-md md:text-lg mt-2">
+                      Name: {item.name}
+                    </p>
+                    <p className="font-mainText text-main text-md md:text-lg">
+                      Price: {item.price}
+                    </p>
+                    <p className="font-mainText text-main text-md md:text-lg">
+                      Color: {item.color}
+                    </p>
+                    <p className="font-mainText text-main text-md md:text-lg mb-2">
+                      Quantity: {item.quantity}
+                    </p>
+                  </div>
+                )}
+                {Object.keys(item).length >= 5 && (
+                  <div className="border-t-4 border-accent">
+                    <p className="font-mainText text-main text-md md:text-lg mt-2">
+                      Name: {item.name}
+                    </p>
+                    <p className="font-mainText text-main text-md md:text-lg">
+                      Size: {item.size}
+                    </p>
+                    <p className="font-mainText text-main text-md md:text-lg">
+                      Price: {item.price}
+                    </p>
+                    <p className="font-mainText text-main text-md md:text-lg">
+                      Color: {item.color}
+                    </p>
+                    <p className="font-mainText text-main text-md md:text-lg">
+                      Type: {item.type}
+                    </p>
+                    <p className="font-mainText text-main text-md md:text-lg mb-2">
+                      Text: {item.text}
+                    </p>
+                  </div>
+                )}
               </div>
             ))}
-            <p className="font-mainText text-main text-lg">
-              Total: ${order.totalPrice}
-            </p>
+            <div className="border-t-4 border-accent">
+              <p className="font-mainText text-main text-lg mt-2">
+                Total: ${order.totalPrice}
+              </p>
+            </div>
             <p className="font-mainText text-main text-lg">
               Ordered on: {order.date.substring(0, 10)}
             </p>
